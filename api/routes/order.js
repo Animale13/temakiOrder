4e70e3666353a70ade24dd0d6c2a2161ff6fc754
g.minoccari <g.minoccari@gmail.com>
2019-10-09 16:05:17 +0200

    Aggiunta pagina iniziale di login, e salvataggio degli ordini del singolo utente

--- api/routes/order.js
+++ api/routes/order.js
@@ -3,13 +3,19 @@ var fs = require('fs');
 var router = express.Router();
 
 var orderComplete = new Array(100);
-orderComplete.fill(0);
+var usersOrder = new Map();
 
 router.post('/', function(req, res, next) {
     console.log('Incoming request');
-    var order = req.body.code;
-    var sign = req.body.change;
-    orderComplete[order] = orderComplete[order] + sign;
+    var orderUpdate = req.body;
+    console.log(orderUpdate.user);
+    usersOrder.set(orderUpdate.user, orderUpdate.items);
+    orderComplete.fill(0);
+    for( var value of usersOrder.values()){
+        for (let i = 0; i < orderComplete.length; i++) {
+            orderComplete[i] = orderComplete[i] + value[i];
+        }
+    }
     console.log(orderComplete);
     res.json(orderComplete);
 });
@@ -19,6 +25,17 @@ router.post('/reset/', function(req, res, next) {
     orderComplete.fill(0);
     console.log(orderComplete);
     res.json(orderComplete);
+});
+
+router.post('/userMenu/', function(req, res, next){
+    console.log(req.body.user);
+    let user = req.body.user;
+    console.log(usersOrder.has(user));
+    if(usersOrder.has(user)){
+        console.log("Found");
+        res.json(usersOrder.get(user));
+    }
+
 });
 
 module.exports = router;
